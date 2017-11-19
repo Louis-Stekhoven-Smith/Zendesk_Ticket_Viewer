2d4c3ff08c7047becf1df280a09fd8ba31592a9a
Louis <louis.stekhoven@gmail.com>
2017-11-19 20:05:06 +1100

    Setup project/Add Travis CI Integration

---
+++ app/main.py
@@ -0,0 +1,15 @@
+
+def placeholder():
+    print("this is a test")
+    return True
+
+def main():
+
+    placeholder()
+
+    return True
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ tests/test_travisSetUp.py
@@ -0,0 +1,7 @@
+# A test to check travis CI is working.
+import app.main
+
+def test_dummyTest():
+    assert(app.main.placeholder())
+
+
