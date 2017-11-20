71ca1cf7ce176e6bb27626d75842e5026a3feab1
Louis <louis.stekhoven@gmail.com>
2017-11-21 00:19:53 +1100

    Created Ticket Class
    implemented basic display to command line using __str__

---
+++ zendesk_ticket_viewer/ticket.py
@@ -0,0 +1,23 @@
+import abc
+class Ticket(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def __init__(self,data ):
+        pass
+
+    @abc.abstractmethod
+    def __str__(self):
+        pass
+
+class ZendeskTicket(Ticket):
+
+    def __init__(self,data):
+        self.id = data['ticket']['id']
+        self.subject = data['ticket']['subject']
+        self.description = data['ticket']['description']
+        self.updated_at = data['ticket']['updated_at']
+
+    def __str__(self):
+        return'\n {} \n {} \n {} \n {}' .format(self.id, self.subject,self.description,self.updated_at)
+
+
