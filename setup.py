58710773d2abd6596fb255c90406b01071d77ae1
Louis <louis.stekhoven@gmail.com>
2017-11-22 07:46:37 +1100

    Updated setup.py meta data

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='zendesk_ticket_viewer',
     version=0.1,
     description='A command line tool to view Zendesk tickets',
+    long_description='Displayed on pyPi prokect page.',
     author='Louis Stekhoven-Smith',
     author_email='louis.stekhoven@gmail.com',
-    packages=['zendesk_ticket_viewer', 'tests']
+    packages=find_packages(),
+    install_requires=['requests','zdesk','pytest','setuptools'],
+    license='GPL-3.0'
 )
