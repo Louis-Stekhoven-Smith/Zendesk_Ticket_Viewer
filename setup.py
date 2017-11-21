03607ef4c9c9569e92cc65623f6275989d6698ca
Louis <louis.stekhoven@gmail.com>
2017-11-22 10:15:15 +1100

    Added ReadTheDocs skeleton code - still needs to be filled out

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='zendesk_ticket_viewer',
     version=0.1,
     description='A command line tool to view Zendesk tickets',
-    long_description='Displayed on pyPi prokect page.',
+    long_description='Displayed on pyPi project page.',
     author='Louis Stekhoven-Smith',
     author_email='louis.stekhoven@gmail.com',
     packages=find_packages(),
