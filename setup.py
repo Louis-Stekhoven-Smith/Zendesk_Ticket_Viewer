6aec4706a67da03d02038ea7b7402ba2bcd3139b
Louis <louis.stekhoven@gmail.com>
2017-11-22 11:57:56 +1100

    Accidentally included setuptools as a required dependency in setup
    This resulted in the egg being installed into system packages
    instead of the package

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='zendesk_ticket_viewer',
-    version=0.1,
+    version=0.3,
     description='A command line tool to view Zendesk tickets',
     long_description='Displayed on pyPi project page.',
     author='Louis Stekhoven-Smith',
     author_email='louis.stekhoven@gmail.com',
     packages=['zendesk_ticket_viewer'],
-    install_requires=['requests','zdesk','pytest','setuptools'],
+    install_requires=['requests','zdesk','pytest'],
     license='GPL-3.0',
     zip_safe=False
 )
